94f946e91f2de9e87bd9b582d908826efac9aef3
Adriaan de Jongh <5611323+AdriaandeJongh@users.noreply.github.com>
2026-07-01 09:47:14 +0200

    only update offset after dragging from filesystem – not when dragging within scenes

--- addons/sprite_offset_tools/plugin.gd
+++ addons/sprite_offset_tools/plugin.gd
@@ -4,6 +4,8 @@ extends EditorPlugin
 var sprite_offset_editor: EditorDock
 var sprite_2d_inspector: EditorInspectorPlugin
 
+var _is_dragging_from_file_system: bool = false
+
 
 func _enter_tree() -> void:
 	sprite_offset_editor = preload("uid://ngrx3lxeecv3").instantiate()
@@ -20,22 +22,25 @@ func _exit_tree() -> void:
 
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_DRAG_BEGIN:
-		get_tree().node_added.connect(_on_node_added)
-	elif what == NOTIFICATION_DRAG_END:
+		if get_viewport().gui_get_drag_data()["type"] == "files":
+			_is_dragging_from_file_system = true
+			get_tree().node_added.connect(_on_node_added)
+	elif what == NOTIFICATION_DRAG_END and _is_dragging_from_file_system:
+		_is_dragging_from_file_system = false
 		get_tree().node_added.disconnect(_on_node_added)
 
 
 func _on_node_added(node: Node):
-	var sprite := node as Sprite2D
+	var sprite: Sprite2D = node as Sprite2D
 	if not sprite:
 		return
 	
 	if not EditorInterface.get_edited_scene_root() or not EditorInterface.get_edited_scene_root().is_ancestor_of(sprite):
 		return
 	
-	update_offset(sprite)
+	_update_offset(sprite)
 
 
-func update_offset(sprite: Sprite2D) -> void:
+func _update_offset(sprite: Sprite2D) -> void:
 	await get_editor_interface().get_base_control().get_tree().process_frame
 	SpriteOffsetDatabase.update_offset(sprite, true)
