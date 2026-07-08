8be753b331265df806a46448ebe117ff69445c75
Adriaan de Jongh <5611323+AdriaandeJongh@users.noreply.github.com>
2026-07-08 12:35:25 +0200

    improved database API function descriptions

--- addons/sprite_offset_tools/sprite_offset_database.gd
+++ addons/sprite_offset_tools/sprite_offset_database.gd
@@ -5,7 +5,7 @@ extends Object
 const DATABASE_PATH = "res://sprite_offset_database.dat"
 
 static var _db_initialized: bool = false
-static var _db: Dictionary[String, Vector2] = {}
+static var _db: Dictionary[String, Vector2] = {} # [UID, Offset]
 
 
 ## Loads the database into cache.
@@ -38,31 +38,43 @@ static func has_offset_for_texture_uid(texture_uid: String) -> bool:
 	return _db.has(texture_uid)
 
 
-## Get the offset for a texture.
+## Get the offset for a texture. Note that the offsets in the database are relative to
+## the left top corner; not the center. If you want to use the offset returned by
+## this function for a Sprite2D with the 'centered' property enabled, simply
+## add `texture.get_size() * 0.5.
 static func get_offset_for_texture(texture: Texture2D) -> Vector2:
 	return get_offset_for_texture_uid(ResourceUID.path_to_uid(texture.resource_path))
 
 
-## Get the offset for a texture.
+## Get the offset for a texture. Note that the offsets in the database are relative to
+## the left top corner; not the center. If you want to use the offset returned by
+## this function for a Sprite2D with the 'centered' property enabled, simply
+## add `texture.get_size() * 0.5.
 static func get_offset_for_texture_uid(texture_uid: String) -> Vector2:
 	if not _db_initialized:
 		_load_database()
 	return _db.get(texture_uid, Vector2.ZERO)
 
 
-## Set the offset for a texture. Offset should be relative to the left top corner; not the center. If Sprite2D.centered is enabled, simply substract `texture.get_size() * 0.5` from the offset.
+## Set the offset for a texture. The offsets in the database are relative to
+## the left top corner; not the center. If Sprite2D.centered is enabled, simply
+## substract `texture.get_size() * 0.5` from the offset.
 static func set_offset_for_texture(texture: Texture2D, offset: Vector2) -> void:
 	set_offset_for_texture_uid(ResourceUID.path_to_uid(texture.resource_path), offset)
 
 
-## Set the offset for a texture. Offset should be relative to the left top corner; not the center. If Sprite2D.centered is enabled, simply substract `texture.get_size() * 0.5` from the offset.
+## Set the offset for a texture. The offsets in the database are relative to
+## the left top corner; not the center. If Sprite2D.centered is enabled, simply
+## substract `texture.get_size() * 0.5` from the offset.
 static func set_offset_for_texture_uid(texture_uid: String, offset: Vector2) -> void:
 	if not _db_initialized:
 		_load_database()
 	_db[texture_uid] = offset
 
 
-## Update a Sprite2D's offset to the offset found in the database. If `preserve_position` is `true`, the Sprite2D's position will also be updated to counteract the visual repositioning of the sprite.
+## Update a Sprite2D's offset to the offset found in the database. If `preserve_position`
+## is `true`, the Sprite2D's position will also be updated to counteract the visual
+## repositioning of the sprite.
 static func update_offset(sprite: Sprite2D, preserve_position: bool) -> void:
 	if not _db_initialized:
 		_load_database()
@@ -86,7 +98,8 @@ static func update_offset(sprite: Sprite2D, preserve_position: bool) -> void:
 		sprite.position -= sprite.offset - prev_offset
 
 
-## Clean up the database by removing 
+## Clean up the database by removing items in the dictionary that refer to UIDs that
+## are no longer in the project
 static func cleanup() -> void:
 	if not _db_initialized:
 		_load_database()
